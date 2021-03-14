004056ec939ec595272d6f96b5200a0f9edd9f79
Borko <petrovicborko392@gmail.com>
2021-03-14 19:45:13 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import "./App.css";
 import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
-import Home from "./Components/Home/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home/>
-          </Route>
-        </Switch>
       </Router>
     </div>
   );
