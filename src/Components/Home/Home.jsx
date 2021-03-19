fa0a4cc62e936b67e73fe586f55d66b4aeeb1990
Borko <petrovicborko392@gmail.com>
2021-03-19 23:21:32 +0100

    Media Queries

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,13 +6,12 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { Link } from "react-router-dom";
 import Section2 from "./Section2";
-import Container from "@material-ui/core/Container";
 import "./Home.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    padding: "280px 0px 280px 0px ",
     backgroundColor: "#F9F1E9",
-    height: "100vh",
     display: "grid",
     justifyContent: "center",
     alignContent: "center",
@@ -29,59 +28,56 @@ export default function Home() {
 
   return (
     <div className="Home">
-      <Container disableGutters={true} maxWidth={false}>
-        <div className={classes.root}>
-          <Grid container>
-            <Grid item xs={12} sm={2}>
-              <Paper></Paper>
-            </Grid>
-            <Grid item xs={12} sm={8}>
-              <Paper className={classes.paper}>
-                <Typography variant="h4" color="primary">
-                  It's time to understand your customer's behavior as never
-                  before
-                </Typography>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} sm={2}>
-              <Paper></Paper>
-            </Grid>
+      <div className={classes.root}>
+        <Grid container>
+          <Grid item xs={12} sm={2}>
+            <Paper></Paper>
           </Grid>
-          <Grid container>
-            <Grid item xs={12} sm={2}>
-              <Paper></Paper>
-            </Grid>
-            <Grid item xs={12} sm={8}>
-              <Paper className={classes.paper}>
-                <Typography color="secondary">
-                  Impala is the first neuromarketing DIY platform that enables
-                  you to gain a deep understanding of human behavior. It
-                  provides you with out of the box data to take your marketing
-                  strategy to the next level.
-                </Typography>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} sm={2}>
-              <Paper></Paper>
-            </Grid>
+          <Grid item xs={12} sm={8}>
+            <Paper className={classes.paper}>
+              <Typography variant="h4" color="primary">
+                It's time to understand your customer's behavior as never before
+              </Typography>
+            </Paper>
           </Grid>
-          <Grid container>
-            <Grid item xs={12} sm={2}>
-              <Paper></Paper>
-            </Grid>
-            <Grid item xs={12} sm={8}>
-              <Paper className={classes.paper}>
-                <Link to="/Features">
-                  <Button>Find out how</Button>
-                </Link>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} sm={2}>
-              <Paper></Paper>
-            </Grid>
+          <Grid item xs={12} sm={2}>
+            <Paper></Paper>
           </Grid>
-        </div>
-      </Container>
+        </Grid>
+        <Grid container>
+          <Grid item xs={12} sm={2}>
+            <Paper></Paper>
+          </Grid>
+          <Grid item xs={12} sm={8}>
+            <Paper className={classes.paper}>
+              <Typography color="secondary">
+                Impala is the first neuromarketing DIY platform that enables you
+                to gain a deep understanding of human behavior. It provides you
+                with out of the box data to take your marketing strategy to the
+                next level.
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <Paper></Paper>
+          </Grid>
+        </Grid>
+        <Grid container>
+          <Grid item xs={12} sm={2}>
+            <Paper></Paper>
+          </Grid>
+          <Grid item xs={12} sm={8}>
+            <Paper className={classes.paper}>
+              <Link to="/Features">
+                <Button>Find out how</Button>
+              </Link>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={2}>
+            <Paper></Paper>
+          </Grid>
+        </Grid>
+      </div>
 
       <Section2 />
     </div>
--- src/Components/Home/Section2.jsx
+++ src/Components/Home/Section2.jsx
@@ -5,12 +5,12 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Impala from "../../Assets/Impala.png";
 import { Button } from "@material-ui/core";
-import Container from "@material-ui/core/Container";
+import { Link } from "react-router-dom";
 import "./Home.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: "150vh",
+    padding: "150px 0px 150px 0px ",
     backgroundColor: "#ffffff",
     display: "grid",
     justifyContent: "center",
@@ -41,101 +41,102 @@ const Section2 = (props) => {
   const classes = useStyles();
 
   return (
-    <Container disableGutters={true} maxWidth={false}>
-      <div className={classes.root}>
-        <Grid container>
-          <Grid item xs={12} sm={3}>
-            <Paper></Paper>
-          </Grid>
-          <Grid item xs={12} sm={6} >
-            <Paper className={classes.paper}>
-              <Typography variant="h4" color="primary">
-                Why Impala?
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={3}>
-            <Paper></Paper>
-          </Grid>
+    <div className={classes.root}>
+      <Grid container>
+        <Grid item xs={12} sm={3}>
+          <Paper></Paper>
         </Grid>
-        <Grid container>
-          <Grid item xs={12} sm={3}>
-            <Paper></Paper>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Paper className={classes.paper}>
-              <Typography color="secondary">
-                <p>
-                  Digital media and digital marketing are moving faster than
-                  ever. It’s not only impossible to keep track of all trends
-                  it’s even more challenging to understand what trends are
-                  matching your target customer.
-                </p>
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={3}>
-            <Paper></Paper>
-          </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+            <Typography variant="h4" color="primary">
+              Why Impala?
+            </Typography>
+          </Paper>
         </Grid>
-        <Grid container>
-          <Grid item xs={12} sm={3}>
-            <Paper></Paper>
-          </Grid>
-          <Grid item xs={12} sm={6}>
-            <Paper className={classes.paper}>
-              <img className={classes.img} src={Impala} alt="logo" />
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={3}>
-            <Paper></Paper>
-          </Grid>
+        <Grid item xs={12} sm={3}>
+          <Paper></Paper>
         </Grid>
-        <Grid className={classes.textAlign} container>
-          <Grid item xs={12} sm={3}>
-            <Paper></Paper>
-          </Grid>
-          <Grid item xs={12} sm={3}>
-            <Paper className={classes.paper}>
-              <Typography color="primary">
-                <p className={classes.fontSize}>
-                  The most reliable approach, nowadays, is to understand your
-                  target audience. Better than you ever have before. And better
-                  than they could ever understand themselves.
-                </p>
-                <Button href="/Services" className={classes.BtnColor}>
-                  Services
-                </Button>
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={3}>
-            <Paper className={classes.paper}>
-              <Typography color="secondary">
-                <p>
-                  That’s where neuromarketing comes in. Being one of the top
-                  trends itself (for good reasons!) neuromarketing focuses on
-                  the in-depth human behavior by using the latest implicit
-                  measurement techniques.
-                </p>{" "}
-                <p>
-                  {" "}
-                  Those highly advanced technologies enable you to define the
-                  drivers of behavior – beyond the conscious behavior of your
-                  customers. So far, and because of its complexity,
-                  neuromarketing analysis was mainly managed by experts that
-                  would provide you with selected results.
-                </p>{" "}
-                <p>Till IMPALA.</p>
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={12} sm={3}>
-            <Paper></Paper>
-          </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item xs={12} sm={3}>
+          <Paper></Paper>
         </Grid>
-      </div>
-    </Container>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+            <Typography color="secondary">
+              <p>
+                Digital media and digital marketing are moving faster than ever.
+                It’s not only impossible to keep track of all trends it’s even
+                more challenging to understand what trends are matching your
+                target customer.
+              </p>
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Paper></Paper>
+        </Grid>
+      </Grid>
+      <Grid container>
+        <Grid item xs={12} sm={3}>
+          <Paper></Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper className={classes.paper}>
+            <img className={classes.img} src={Impala} alt="logo" />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Paper></Paper>
+        </Grid>
+      </Grid>
+      <Grid className={classes.textAlign} container>
+        <Grid item xs={12} sm={3}>
+          <Paper></Paper>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Paper className={classes.paper}>
+            <Typography color="primary">
+              <p className={classes.fontSize}>
+                The most reliable approach, nowadays, is to understand your
+                target audience. Better than you ever have before. And better
+                than they could ever understand themselves.
+              </p>
+              <Link to="/Services">
+              <Button href="/Services" className={classes.BtnColor}>
+                Services
+              </Button>
+              </Link>
+            
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Paper className={classes.paper}>
+            <Typography color="secondary">
+              <p>
+                That’s where neuromarketing comes in. Being one of the top
+                trends itself (for good reasons!) neuromarketing focuses on the
+                in-depth human behavior by using the latest implicit measurement
+                techniques.
+              </p>{" "}
+              <p>
+                {" "}
+                Those highly advanced technologies enable you to define the
+                drivers of behavior – beyond the conscious behavior of your
+                customers. So far, and because of its complexity, neuromarketing
+                analysis was mainly managed by experts that would provide you
+                with selected results.
+              </p>{" "}
+              <p>Till IMPALA.</p>
+            </Typography>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Paper></Paper>
+        </Grid>
+      </Grid>
+    </div>
   );
 };
 
