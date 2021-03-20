d17c66adeef3de1ac4939d16aba6b49427f43649
Borko <petrovicborko392@gmail.com>
2021-03-20 01:46:34 +0100

    Update Beneftis.jsx

--- src/SharedComponents/Beneftis.jsx
+++ src/SharedComponents/Beneftis.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import "../Home.css";
-
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 const useStyles = makeStyles((theme) => ({
