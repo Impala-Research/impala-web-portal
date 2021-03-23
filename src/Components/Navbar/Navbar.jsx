c12b6334f3c676285ad96788209d0096c5f31dbe
Borko <petrovicborko392@gmail.com>
2021-03-23 19:37:20 +0100

    Navbar Hover

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
+import { AppBar, Toolbar, Typography, makeStyles } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import logo from "../../Assets/Logo-img.png";
+
 const useStyles = makeStyles(() => ({
   appBar: {
     backgroundColor: "#F4E3D4",
@@ -19,39 +17,38 @@ const useStyles = makeStyles(() => ({
     color: "#707070",
   },
 }));
+
 function Navbar() {
   const classes = useStyles();
   return (
-    <div className="Navbar1">
-      <AppBar position="static" className={classes.appBar}>
-        <Toolbar>
-          <Link to="/">
-            <img src={logo} alt="logo" />
+    <AppBar position="static" className={classes.appBar}>
+      <Toolbar>
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
+        <Typography className={classes.title}></Typography>
+        <Typography className={classes.links}>
+          <Link className={classes.links} to="/">
+            Home
           </Link>
-          <Typography className={classes.title}></Typography>
-          <Typography className={classes.links}>
-            <Link className={classes.links} to="/">
-              Home
-            </Link>
-            <Link className={classes.links} to="/Services">
-              Services
-            </Link>
-            <Link className={classes.links} to="/Features">
-              Features
-            </Link>
-            <Link className={classes.links} to="/Plans-Pricing">
-              Plans And Pricing
-            </Link>
-            <Link className={classes.links} to="/Team">
-              Team
-            </Link>
-            <Link className={classes.links} to="/Contact">
-              Contact
-            </Link>
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
+          <Link className={classes.links} to="/Services">
+            Services
+          </Link>
+          <Link className={classes.links} to="/Features">
+            Features
+          </Link>
+          <Link className={classes.links} to="/Plans-Pricing">
+            Plans And Pricing
+          </Link>
+          <Link className={classes.links} to="/Team">
+            Team
+          </Link>
+          <Link className={classes.links} to="/Contact">
+            Contact
+          </Link>
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 }
 export default Navbar;
--- src/SharedComponents/Beneftis.jsx
+++ src/SharedComponents/Beneftis.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import { Typography } from "@material-ui/core";
+import { Grid, Paper, Typography, makeStyles } from "@material-ui/core";
 import EyeImage from "../Assets/Eye-img.png";
 import "../../src/Components/Home/Home/Home.css";
 
